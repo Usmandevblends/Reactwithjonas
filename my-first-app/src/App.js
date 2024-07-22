f42a126f5b53d60c6f67aa1b60b20a74e3d62ccd
MuhammadUsman <imdevusman@gmail.com>
2024-07-22 16:25:40 +0500

    add some style and funcationslity

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import Message from "./components/Message";
+
 function App() {
-const [advice, setAdvice] = useState('');
-const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
     setAdvice(data.slip.advice);
     setCount((c) => c + 1);
-    
   }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
     <>
       <center>
         <h1>{advice}</h1>
         <button onClick={getAdvice}>Get advice</button>
-        <p>You have read <strong>{count}</strong> pieces of advice</p>
+        <Message count={count} />
       </center>
     </>
   );
---
+++ my-first-app/src/components/Message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message(props) {
+  return (
+    <>
+      <p> You have read <strong>{props.count}</strong> pieces of advice</p>
+    </>
+  );
+}
+
+export default Message;
